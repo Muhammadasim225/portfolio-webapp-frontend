00d33ffb94a9dbfcc484c37a8dceeec3d465c5ca
Muhammad Asim <muhammadasim4927@gmail.com>
2025-09-10 18:05:51 +0500

    Update Homepage.jsx

--- portfolio-app/src/components/Homepage.jsx
+++ portfolio-app/src/components/Homepage.jsx
@@ -53,7 +53,7 @@ const vari = {
     
       <div id="homepage" className="mt-10 md:flex-col-reverse items-center sm:flex-col-reverse flex-col-reverse lg:mt-10 sm:px-16 px-14 sm:pt-20 pt-16 sm:pl-16 pl-14 sm:pr-16 pr-14 sm:pb-10 pb-8 flex   lg:justify-left justify-center sm:items-center lg:flex lg:flex-row lg:items-center lg:w-full lg:justify-between lg:px-16 lg:pt-16 lg:pl-16 lg:pr-16 lg:pb-10">
         {/* Adjusted the width to take more space */}
-        <motion.div  variants={container} animate="animate"  className=" headline flex sm:flex-col flex-col  first lg:w-3/5 lg:space-y-4  sm:space-y-2 space-y-1 sm:w-full text-black sm:mt-0">
+        <motion.div  variants={container} animate="animate"  className="pb-5 headline flex sm:flex-col flex-col  first lg:w-3/5 lg:space-y-4  sm:space-y-2 space-y-1 sm:w-full text-black sm:mt-0">
           <motion.h1 variants={vari} initial="initial" animate="anima"  className='lg:font-extrabold lg:text-5xl lg:text-black sm:font-extrabold sm:text-black sm:leading-tight sm:text-3xl sm:items-center sm:text-center text-2xl text-center lg:text-left md:text-4xl'>
             Hi, I Am Muhammad Asim,
           </motion.h1>
